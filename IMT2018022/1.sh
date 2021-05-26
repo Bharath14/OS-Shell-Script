42be0557925161d9c38faa47d0cb5c7d24f93df6
Bharath14 <bharathsaiellanti@gmail.com>
2021-05-26 15:32:07 +0530

    Final Commit

--- IMT2018022/1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Program Number: 1
-#Student Name:Ellanti Bharath Sai; 
-#Register Number: IMT2018022
-#Date: 12-01-2021 
-#Description: This program will output the count of block device files in /dev directory.    
-ls /dev/ -l|grep -c ^b 
